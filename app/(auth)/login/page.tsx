8e4c6b9f7db0059746599c41101c8d5ca693a58d
Shourya <shouryaagrawal2806@gmail.com>
2025-06-29 12:57:20 +0530

    18

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -3,10 +3,8 @@
 import Link from 'next/link'; 
 import { signIn } from "next-auth/react"; 
 import { useState } from 'react';
-import { useRouter } from 'next/navigation';
 
 export default function Page() {
-  const router = useRouter();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState<string | null>(null);
